cc95cdb0afa94b484bbdc9d1371f72930771f5fe
MAXSTD <ruhan.bd04@gmail.com>
2017-02-17 16:11:42 +0600

    Progress 29 (Writing More Telling less)

---
+++ index.js
@@ -0,0 +1,19 @@
+//piece of code that does 1 or more action is called function
+//D.R.Y="Do Not Repeat yourself"
+function Added_num(m,s) {
+  return m+s;
+
+}
+
+var answer= Added_num(1,2);
+
+alert("answer")
+
+//Break from The Above text
+
+function go() {
+  alert("Hey");
+}
+go()
+
+var list=["apple",12,go];
